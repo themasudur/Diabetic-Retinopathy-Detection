4f2a7e445c66289d691bea8b4f5f502109d714b0
Mohammad Masudur Rahman <masudurism@gmail.com>
2024-12-09 20:47:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 def has_key(term, text):
     if term in text:
         return 1
     else:
         return 0
-
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+        
 idg = ImageDataGenerator(
     rescale=1./255,
     rotation_range=20,
@@ -24,33 +24,21 @@ idg = ImageDataGenerator(
 )
 idg_test = ImageDataGenerator(rescale=1./255)
 
-
 idg.fit(x_train)
 idg.fit(x_val)
 idg_test.fit(x_test)
-
 
 train = (x_train, y_train_cat)
 validation = (x_val, y_val_cat)
 test = (x_test, y_test_cat)
 
-
 # Model
-print('==> Building model..')
+print('Building model...')
 # net = VGG('VGG19')
 # net = ResNet18()
-# net = PreActResNet18()
-# net = GoogLeNet()
 # net = DenseNet121()
 # net = ResNeXt29_2x64d()
-# net = MobileNet()
-# net = MobileNetV2()
-# net = DPN92()
-# net = ShuffleNetG2()
-# net = SENet18()
-# net = ShuffleNetV2(1)
 # net = EfficientNetB0()
-# net = RegNetX_200MF()
 net = SimpleDLA()
 net = net.to(device)
 if device == 'cuda':
@@ -70,7 +58,6 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=args.lr,
                       momentum=0.9, weight_decay=5e-4)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
-
 
 # Training
 def train(epoch):
@@ -94,7 +81,6 @@ def train(epoch):
 
         progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                      % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
-
 
 def test(epoch):
     global best_acc
@@ -129,7 +115,6 @@ def test(epoch):
             os.mkdir('checkpoint')
         torch.save(state, './checkpoint/ckpt.pth')
         best_acc = acc
-
 
 for epoch in range(start_epoch, start_epoch+200):
     train(epoch)
